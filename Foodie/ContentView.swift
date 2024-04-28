8740506948f7ac9eff800a4fe56de048decdb223
Anuj <anujjhace@gmail.com git config --global user.name anuj>
2024-04-28 09:10:07 +0530

    Initial Commit

---
+++ Foodie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Foodie
+//
+//  Created by ANUJ KUMAR on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Foodie/FoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieApp.swift
+//  Foodie
+//
+//  Created by ANUJ KUMAR on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
